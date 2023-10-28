fbac9a48394e731914b2ef89c53adfaf384e044d
rashmirn <rashmirn.sahoo@gmail.com>
2023-10-28 17:11:30 +0530

    initial commit

---
+++ dcelery/dcelery/celery.py
@@ -0,0 +1,14 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dcelery.settings')
+
+app = Celery("dcelery")
+
+app.config.from_object("django.conf.settings", namespce="CELERY")
+
+@app.task
+def add_number():
+    return
+
+app.autodiscover_tasks()
