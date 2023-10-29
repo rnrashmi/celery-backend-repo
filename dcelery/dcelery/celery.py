379201ea4745800c8db0b58d40e5f821139befac
rashmirn <rashmirn.sahoo@gmail.com>
2023-10-29 20:11:25 +0530

    celery config fix 2

--- dcelery/dcelery/celery.py
+++ dcelery/dcelery/celery.py
@@ -5,7 +5,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dcelery.settings')
 
 app = Celery("dcelery")
 
-app.conf.from_object("django.conf.settings", namespce="CELERY")
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 @app.task
 def add_number():
